ee4203c2a3d893689b29a9193ee614ad919f56f3
Charlie Pauvré <chpauvre@gmail.com>
2020-10-29 01:16:26 +0100

    init v3

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@ap
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:5000'
+	uri: 'https://social-app-graphql-site.herokuapp.com/'
 });
 
 const authLink = setContext((req, res) => {
